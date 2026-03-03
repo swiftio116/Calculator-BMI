9bfb5baa9a5fcf466c54c2c6219f306138ac2db4
Aiaz <aizmyza19961906@gmail.com>
2026-03-03 11:24:37 +0300

    Initial Commit

---
+++ BMITests/BMITests.swift
@@ -0,0 +1,17 @@
+//
+//  BMITests.swift
+//  BMITests
+//
+//  Created by Muzafarov on 03.03.2026.
+//
+
+import Testing
+@testable import BMI
+
+struct BMITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
